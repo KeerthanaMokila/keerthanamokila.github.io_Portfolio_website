c76b5a362ae92f2b450c6fd808b218c8e2429dda
Keerthana <keerthanamokila3@gmail.com>
2026-06-07 19:01:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Cloud Enthusiast",
+        "Kubernetes Learner",
+        "Azure Learner",
+        "CI/CD Enthusiast",
+        "Linux Explorer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1200,
+    loop: true
+});
